d79798e077688370f2c13014317ba50077820046
Charlcharlo <Charlcharlo@gmail.com>
2023-03-17 08:48:22 +0200

    Start keyboard functionality in results

---
+++ src/components/Result.jsx
@@ -0,0 +1,9 @@
+export default function Result({renderResult, mon}) {
+    return (
+        <div onClick={() => {
+            renderResult(mon);
+        }}>
+            <p>{mon.displayName}</p>
+        </div>
+    )
+}
